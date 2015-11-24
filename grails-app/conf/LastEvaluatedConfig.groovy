b3d85f27a3aa76590a64b04823f15df23c883d27
m-r-c <m-r-c@users.noreply.github.com>
2015-11-24 16:20:14 +1100

    https://github.com/AtlasOfLivingAustralia/biocollect/issues/148 environment-specific wordpress home pages for Biocollect

--- grails-app/conf/LastEvaluatedConfig.groovy
+++ grails-app/conf/LastEvaluatedConfig.groovy
@@ -5,5 +5,3 @@ spatial.wms.url = "${spatial.baseURL}/geoserver/ALA/wms?"
 spatial.wms.cache.url = "${spatial.baseURL}/geoserver/gwc/service/wms?"
 ecodata.service.url = "${ecodata.baseURL}/ws"
 upload.images.url = "${grails.serverURL}/image?id="
-//TODO: Update to a correct wordpress homepage url.
-biocollect.homepageUrl = "${ala.baseURL}/get-involved/citizen-science/"
