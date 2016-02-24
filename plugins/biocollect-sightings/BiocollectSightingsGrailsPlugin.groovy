6f45a6dbe83c29bb3891ad554c744a84259a0524
sat01a <sathish.sathyamoorthy@gmail.com>
2016-02-25 09:26:57 +1100

    Version update

--- plugins/biocollect-sightings/BiocollectSightingsGrailsPlugin.groovy
+++ plugins/biocollect-sightings/BiocollectSightingsGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class BiocollectSightingsGrailsPlugin {
     // the plugin version
-    def version = "1.0-SNAPSHOT"
+    def version = "1.1-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.4 > *"
     // resources that are excluded from plugin packaging
