2c6bc1393371de29acb94930abde285c590c687b
temi <temi.varghese@csiro.au>
2018-07-28 20:26:29 +1000

    added reference to un-minified jquery ui file

--- grails-app/assets/javascripts/base.js
+++ grails-app/assets/javascripts/base.js
@@ -3,9 +3,7 @@
 //= require jquery-migrate-1.2.1.min.js
 //= require jquery.cookie/jquery.cookie.js
 //jquery ui
-//=requirejquery-ui/jquery-ui-1.11.2-no-autocomplete.js
-//=requirejquery.autocomplete.js
-//= require jquery-ui/1.9.2/jquery-ui-1.9.2.custom.min.js
+//= require jquery-ui/1.9.2/jquery-ui-1.9.2.custom.js
 //= require jquery.appear/jquery.appear.js
 //= require bootstrap/js/bootstrap.js
 //= require bootstrap-combobox/bootstrap-combobox.js
