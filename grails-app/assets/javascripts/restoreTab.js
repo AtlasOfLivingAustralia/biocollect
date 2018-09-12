95d807b273014e92fac9ad5358e9fc3aaa91a47d
temi <temi.varghese@csiro.au>
2018-09-12 13:52:33 +1000

    project page is now reloaded when survey/project activity is published. fixed a bug with restore tab fixed a bug which caused preview activity to fail on survey/project activity tab

--- grails-app/assets/javascripts/restoreTab.js
+++ grails-app/assets/javascripts/restoreTab.js
@@ -5,16 +5,25 @@ var RestoreTab = function (tabId, defaultHrefTab) {
 
     //Restore tab reference if available else show default tab.
     var storedTab = amplify.store(tabId + '-state');
-    if (storedTab && $(storedTab).length > 0) {
-        $(storedTab + "-tab").tab('show');
-    } else {
+    if (storedTab && (storedTab.indexOf('#') != 0)) {
+        storedTab = '#' + storedTab;
+    }
+
+    if (storedTab && ($(storedTab).length > 0)) {
+        $(storedTab).tab('show');
+    }
+    else if (defaultHrefTab) {
         $('#' + defaultHrefTab).tab('show');
     }
 
     //Store tab reference
     $('#' + tabId + ' a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-        var tab = e.currentTarget.hash;
-        amplify.store(tabId + '-state', tab);
+        var tab = e.currentTarget.id;
+        saveTabSelection(tabId, tab);
     });
 
+};
+
+function saveTabSelection(uniqueTabId, tabHeadingId) {
+    amplify.store(uniqueTabId + '-state', tabHeadingId);
 };
