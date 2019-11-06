71e248844149ce081a60efd866ed148c0afb2994
Patricia Koh <patkyn@hotmail.com>
2019-11-06 18:20:20 +1100

    https://github.com/AtlasOfLivingAustralia/biocollect/issues/1211 - Use HttpClient for sending file to do pdfgen

--- grails-app/controllers/au/org/ala/biocollect/merit/ResourceController.groovy
+++ grails-app/controllers/au/org/ala/biocollect/merit/ResourceController.groovy
@@ -1,24 +1,14 @@
 package au.org.ala.biocollect.merit
 
 import grails.converters.JSON
-import groovyx.net.http.HTTPBuilder
-import org.apache.http.NameValuePair
-import org.apache.http.client.HttpClient
 
-//import groovyx.net.http.URIBuilder
-import org.apache.http.client.methods.HttpGet
-import org.apache.http.client.params.ClientPNames
-import org.apache.http.client.utils.URIBuilder
-import org.apache.http.impl.client.AbstractHttpClient
-import org.apache.http.impl.client.CloseableHttpClient
-import org.apache.http.impl.client.HttpClientBuilder
-import org.apache.http.impl.client.HttpClients
-import org.apache.http.message.BasicNameValuePair
-import org.apache.http.params.BasicHttpParams
-
-import static groovyx.net.http.ContentType.TEXT
-import static groovyx.net.http.Method.GET
-
+import org.apache.http.HttpHost;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.client.utils.URIUtils;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.impl.client.LaxRedirectStrategy;
 
 class ResourceController {
 
@@ -38,36 +28,33 @@ class ResourceController {
     def pdfUrl() {
         def url = params.file
 
-        def uri = new URIBuilder(grailsApplication.config.pdfgen.baseURL).setPath('api/pdf').setParameter('docUrl', url)
-               // .setParameter("http.protocol.handle-redirects","false")
+        CloseableHttpClient httpclient = HttpClients.custom().setRedirectStrategy(new LaxRedirectStrategy()).build()
 
+        try {
+            HttpClientContext context = HttpClientContext.create()
+            HttpGet httpGet = new HttpGet("${grailsApplication.config.pdfgen.baseURL}/api/pdf?docUrl=" + url)
+            log.debug("Sending file to be converted into pdf: " + httpGet.getRequestLine())
 
-        def http = new HTTPBuilder(uri)
-        //HttpClient ahc = http.client
+            httpclient.execute(httpGet, context)
+            HttpHost target = context.getTargetHost()
+            List<URI> redirectLocations = context.getRedirectLocations()
+            URI location = URIUtils.resolve(httpGet.getURI(), target, redirectLocations)
+            log.debug("Generated pdf location can be obtained from: " + location.toString())
 
-        def location = http.request(GET, TEXT) {
-          //  uri.path = 'api/pdf';
-            //NameValuePair nameValuePair = new NameValuePair().
-           // List<NameValuePair> listParams = new ArrayList<NameValuePair>();
-           // listParams.add(new BasicNameValuePair('docUrl', url))
-           // uri.queryParams = listParams
-           // uri.setParameter('docUrl', url)
-            //uri.queryParams = ['docUrl': url]
-
-        //    uri.query = ['docUrl': url]
-
-            response.success = { rsp ->
-                rsp.headers?.Location
+            if (!location) {
+                def error = ['error': 'error getting pdf url']
+                render error as JSON, status: 500
             }
+
+            def result = ['location': location.toString()]
+            render result as JSON
+
+        } catch (Exception e) {
+            log.error ("Error occurred during pdf generation. " + e.message, e)
+        } finally {
+            httpclient.close()
         }
 
-        if (!location) {
-            def error = ['error': 'error getting pdf url']
-            render error as JSON, status: 500
-        }
-
-        def result = ['location': location]
-        render result as JSON
     }
 
 }
