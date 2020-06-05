21acf02477f531d91136a71b7d57be2699251471
magdziarek <aleksandra.magdziarek@biol.lu.se>
2020-06-05 11:58:23 +0200

    remove redundant code

--- grails-app/assets/javascripts/sites.js
+++ grails-app/assets/javascripts/sites.js
@@ -241,7 +241,6 @@ var SiteViewModel = function (mapContainerId, site, mapOptions) {
     };
 
     function initialiseViewModel() {
-        var overlayLayersMapControlConfig = Biocollect.MapUtilities.getOverlayConfig();
         var baseLayersAndOverlays = Biocollect.MapUtilities.getBaseLayerAndOverlayFromMapConfiguration(fcConfig.mapLayersConfig);
         var options =  {
             autoZIndex: false,
