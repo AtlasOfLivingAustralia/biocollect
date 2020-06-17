5221b75387397b60577e70032e68857f8696efbe
magdziarek <aleksandra.magdziarek@biol.lu.se>
2020-06-17 18:51:12 +0200

    add script required for systematic

--- karma.conf.js
+++ karma.conf.js
@@ -35,6 +35,7 @@ module.exports = function (config) {
             'grails-app/assets/javascripts/organisation.js',
             'grails-app/assets/javascripts/pagination.js',
             'grails-app/assets/javascripts/sites.js',
+            'grails-app/assets/javascripts/systematicSites.js',
             'node_modules/leaflet/dist/leaflet.js',
             'grails-app/assets/vendor/leaflet-plugins-2.0.0/layer/tile/Google.js',
             'grails-app/assets/javascripts/MapUtilities.js',
