6008276cd308ba7829ec003dd7e00b4f721192a3
per305 <Chamali.Perera@csiro.au>
2021-12-01 13:33:48 +1100

    nesp marine artefact upload scripts

---
+++ scripts/RestClient/nesp/marineArtefacts/document.js
@@ -0,0 +1,132 @@
+
+var document =
+    {
+      "labels" : [ ],
+      "dateCreated" : "",
+      "lastUpdated" : "",
+      "isPrimaryProjectImage" : true,
+      "documentId" : "",
+      "filepath" : "2021-11",
+      "thirdPartyConsentDeclarationMade" : false,
+      "projectActivityId" : "",
+      "role" : "information",
+      "name" : "",
+      "type" : "application",
+      "status" : "active",
+      "filename" : "",
+      "isSciStarter" : false,
+      "projectId" : "",
+          "description" : "",
+      "readOnly" : false,
+      "filesize" : "",
+      "contentType" : ""
+}
+
+
+/*
+var document =
+{
+  "documentId" : "",
+  "filename" : "",
+  "filepath" : "2021-02",
+  "isPrimaryProjectImage" : true,
+  "isSciStarter" : false,
+  "labels" : [ ],
+  "name" : "",
+  "projectId" : "",
+  "role" : "mainImage",
+  "status" : "active",
+  "thirdPartyConsentDeclarationMade" : false,
+  "type" : "image",
+  "lng" : "",
+  "filesize" : 104117,
+  "public" : true,
+  "contentType" : "image/jpeg",
+  "lat" : "",
+  "projectActivityId" : "",
+  "roles" : [
+    {
+      "name" : "Information",
+      "id" : "information"
+    },
+    {
+      "name" : "Embedded Video",
+      "id" : "embeddedVideo"
+    },
+    {
+      "name" : "Exceedance Report",
+      "id" : "exceedanceReport"
+    },
+    {
+      "name" : "Photo",
+      "id" : "photo"
+    },
+    {
+      "name" : "Project Plan / Work plan",
+      "id" : "projectPlan"
+    },
+    {
+      "name" : "Project Variation",
+      "id" : "projectVariation"
+    },
+    {
+      "name" : "Project Highlight Report",
+      "id" : "projectHighlightReport"
+    },
+    {
+      "name" : "Other Project document",
+      "id" : "other"
+    }
+  ],
+  "fileReady" : true,
+  "thirdPartyConsentDeclarationRequired" : true,
+  "embeddedVideoVisible" : false,
+  "settings" : {
+    "thirdPartyDeclarationTextSelector" : "#thirdPartyDeclarationText",
+    "roles" : [
+      {
+        "name" : "Information",
+        "id" : "information"
+      },
+      {
+        "name" : "Embedded Video",
+        "id" : "embeddedVideo"
+      },
+      {
+        "name" : "Exceedance Report",
+        "id" : "exceedanceReport"
+      },
+      {
+        "name" : "Photo",
+        "id" : "photo"
+      },
+      {
+        "name" : "Project Plan / Work plan",
+        "id" : "projectPlan"
+      },
+      {
+        "name" : "Project Variation",
+        "id" : "projectVariation"
+      },
+      {
+        "name" : "Project Highlight Report",
+        "id" : "projectHighlightReport"
+      },
+      {
+        "name" : "Other Project document",
+        "id" : "other"
+      }
+    ],
+    "showSettings" : true,
+    "imageLocation" : "/assets/"
+  },
+  "hasPreview" : false,
+  "fileButtonText" : "Change file",
+  "readOnly" : false,
+  "saveHelp" : "You must accept the Privacy Declaration before an image can be made viewable by everyone",
+  "fileLabel" : "",
+  "progress" : 0,
+  "complete" : false,
+  "saveEnabled" : false
+};
+*/
