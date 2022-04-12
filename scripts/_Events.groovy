08aeda361f202c967e4f29b34d133320f839a598
temi <temi.varghese@csiro.au>
2022-04-12 11:12:31 +1000

    refactored code

--- scripts/_Events.groovy
+++
@@ -1,5 +0,0 @@
-eventCreateWarStart = { warName, stagingDir ->
-    ant.propertyfile(file: "${stagingDir}/WEB-INF/classes/application.properties") {
-        entry(key:"app.build", value: new Date().format("dd/MM/yyyy HH:mm:ss"))
-    }
-}
