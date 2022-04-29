dd5f86bccf09475842c69efd0f9ddba3d333e3eb
per305 <Chamali.Perera@csiro.au>
2022-04-29 17:05:29 +1000

    script to soft delet TSR hub documents

--- scripts/RestClient/nesp/cleanNespDocuments.js
+++ scripts/RestClient/nesp/cleanNespDocuments.js
@@ -1,39 +1,26 @@
-var projectIdArr = ['c713e4f1-c57f-4040-9093-24f9aed39ac5','17e82227-aee5-4fbf-adaa-50a0469847e7']
+// This script will soft delete existing NESP TSR hub's documents
+
+var projectIdArr = ['d748bd95-0b4d-450c-8fa4-20a7bcfd9c19','08e07841-764c-4fcd-8bbc-7dc38e6fbd5f']
 
 db.document.update({$and:[{"projectId": {$in: projectIdArr}},{"contentType": "application/pdf"}]},{$set:{"status": "deleted"}},{multi: true})
 
 var documents = db.document.find({"projectId":{$in:projectIdArr}})
 
-var document = ""
-var projectId = ""
-var documentId = ""
-var path = ""
-var record = ""
+var doc = "";
+var documentId = "";
+var filepath = "";
+var filename = "";
 
-var rows = []
+print("DOCUMENT ID, FULL PATH");
 
 while (documents.hasNext()) {
-    document = documents.next();
+    filepath = "/data/ecodata/uploads/"
 
-    projectId = document.projectId;
-    documentId = document.documentId;
-    path = document.filepath;
+    doc = documents.next();
 
-    record = projectId + "," + documentId + "," + path
+    documentId = doc.documentId;
+    filename = doc.filename;
+    filepath += doc.filepath + "/" + filename;
 
-    rows.push(record)
+    print(documentId+", "+filepath);
 }
-
-let csvContent = "data:text/csv;charset=utf-8,";
-
-rows.forEach(function(row) {
-    csvContent += row.join(',');
-    csvContent += "\n";
-});
-
-console.log(csvContent);
-
-var encodedUri = encodeURI(csvContent);
-window.open(encodedUri);
-
-print("documentResult"+documentResult)
