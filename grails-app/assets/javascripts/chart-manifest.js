90dcd64ea5e6580c322c3889d82e51fba63f2243
temi <temi.varghese@csiro.au>
2022-09-27 22:24:32 +1000

    #1478 added time adapter to chartjs to graph entities by year

--- grails-app/assets/javascripts/chart-manifest.js
+++ grails-app/assets/javascripts/chart-manifest.js
@@ -1,5 +1,8 @@
 // jqueryupload
+//= require moment/moment.min.js
+//= require moment/moment-timezone-with-data.min.js
 //= require chartjs/chartV3_8.min.js
+//= require chartjs/adapters/chartjs-adapter-moment.js
 //= require chartjs-plugin-datalabels/chartjs-plugin-datalabels.min.js
 //= require common-bs4.js
 //= require forms-manifest.js
