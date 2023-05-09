dd742ae4b834675e5d2b79350a7832e7a2c981f3
temi <temi.varghese@csiro.au>
2023-05-09 16:58:49 +1000

    #1527 migrating to auth 6.0 getting functional test to work

--- src/main/scripts/runFunctionalTests.sh
+++ src/main/scripts/runFunctionalTests.sh
@@ -22,31 +22,41 @@ if [ ! -d $ECODATA_LOCAL_DIR ]; then
     git clone https://github.com/AtlasOfLivingAustralia/ecodata.git
     cd ecodata
     git checkout $BRANCH
+    echo "Cloned ecodata $BRANCH into /tmp/ecodata"
 else
     cd $ECODATA_LOCAL_DIR
+    git checkout $BRANCH
     git pull
+    echo "Updated ecodata $BRANCH in /tmp/ecodata"
 fi
 
 echo "Dropping database"
-mongo ecodata-functional-test --eval 'db.dropDatabase();'
-mongo ecodata-functional-test --eval 'db.project.count();'
+mongosh ecodata-functional-test --eval 'db.dropDatabase();'
+mongosh ecodata-functional-test --eval 'db.project.count();'
+cd src/integration-test/resources/data_common/
+mongosh ecodata-functional-test loadAlaHub.js
 
 echo "Starting ecodata from `pwd`"
 ls -la
-GRADLE_OPTS="-Xmx512m" ./gradlew bootRun --no-daemon "-Dorg.gradle.jvmargs=-Xmx512m" -Dgrails.env=meritfunctionaltest &
-sleep 120
+GRADLE_OPTS="-Xmx1g" ./gradlew bootRun "-Dorg.gradle.jvmargs=-Xmx1g" -Dgrails.env=meritfunctionaltest &
+sleep 60
 
 cd $MERIT_DIR
-GRADLE_OPTS="-Xmx512m" ./gradlew bootRun --no-daemon "-Dorg.gradle.jvmargs=-Xmx512m" -Dgrails.env=test -Dgrails.server.port.http=8087 &
-sleep 180
+echo "Starting wire mock"
+./gradlew startWireMock &
+sleep 30
 
+echo "Starting biocollect"
+GRADLE_OPTS="-Xmx1g" ./gradlew bootRun "-Dorg.gradle.jvmargs=-Xmx1g" -Dgrails.env=test -Dgrails.server.port.http=8087 &
+sleep 60
 chmod u+x src/main/scripts/loadFunctionalTestData.sh
 
 echo "Running functional tests"
-./gradlew integrationTest --stacktrace -Dgeb.env=$GEB_ENV
+GRADLE_OPTS="-Xmx1g" ./gradlew integrationTest "-Dorg.gradle.jvmargs=-Xmx1g" --stacktrace -Dgeb.env=$GEB_ENV
 
 RETURN_VALUE=$?
 
+kill %3
 kill %2
 kill %1
 
