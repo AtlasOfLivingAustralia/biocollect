91b703c3e8dfc139f81d9f901a53b3bc1efcdee7
Jack Brinkman <jack.brinkman@csiro.au>
2024-04-29 20:55:26 +1000

    Fixed internal server error when attempting to render non-existent staticPage controller 404 view

--- grails-app/controllers/au/org/ala/biocollect/StaticPageController.groovy
+++ grails-app/controllers/au/org/ala/biocollect/StaticPageController.groovy
@@ -17,7 +17,7 @@ class StaticPageController {
             String setting = "${page}";
             render view: 'index', model: ["setting": setting, "mobile": params.mobile ?:false];
         } else {
-            render view: "404";
+            redirect (controller: "hub", action: "index")
         }
     }
 
