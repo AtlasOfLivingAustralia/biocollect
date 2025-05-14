873bdcda7b83cd7020033c7f08f36dfee3735f8d
temi <temi.varghese@csiro.au>
2025-05-14 12:59:00 +1000

    #1704 - added ckeditor to manage hub page - added introductory text

--- src/test/groovy/au/org/ala/biocollect/TemplateTagLibSpec.groovy
+++ src/test/groovy/au/org/ala/biocollect/TemplateTagLibSpec.groovy
@@ -1,22 +1,37 @@
 package au.org.ala.biocollect
 
+import asset.pipeline.grails.AssetMethodTagLib
 import asset.pipeline.grails.AssetProcessorService
 import asset.pipeline.grails.AssetsTagLib
-import asset.pipeline.grails.AssetMethodTagLib
 import grails.converters.JSON
+import grails.testing.web.GrailsWebUnitTest
 import grails.testing.web.taglib.TagLibUnitTest
 import groovy.json.JsonSlurper
 import org.grails.web.converters.marshaller.json.CollectionMarshaller
 import org.grails.web.converters.marshaller.json.MapMarshaller
 import spock.lang.Specification
 
-class TemplateTagLibSpec extends Specification implements TagLibUnitTest<TemplateTagLib> {
+class TemplateTagLibSpec extends Specification implements TagLibUnitTest<TemplateTagLib>, GrailsWebUnitTest {
     AssetProcessorService assetProcessorServiceEx
 
-    def setupSpec() {
+    def setup() {
         JSON.registerObjectMarshaller(new MapMarshaller())
         JSON.registerObjectMarshaller(new CollectionMarshaller())
         mockTagLibs(AssetsTagLib, AssetMethodTagLib)
+//        GrailsWebMockUtil
+//        def mockRequest = new GrailsMockHttpServletRequest()
+//        mockRequest.setRequestURI("/current/page")
+//        mockRequest.setForwardURI("/current/page")
+//        mockRequest.setQueryString("id=123")
+//        def ctx = applicationContext
+//        def mockRequest = bindMockHttpServletRequest()
+//        def mockGrailsWebRequest = GrailsWebMockUtil.bindMockWebRequest()
+//        mockGrailsWebRequest.request.requestURI = "/current/page"
+//        mockGrailsWebRequest.request.forwardURI >> "/current/page"
+//        mockGrailsWebRequest.request.queryString >> "id=123"
+//        tagLib.request.getRequestURI() >> "/current/page"
+//        tagLib.request.getForwardURI() >> "/current/page"
+//        tagLib.request.getQueryString() >> "id=123"
     }
 
     def "test getFilesToPreCacheForPWA"() {
@@ -47,5 +62,66 @@ class TemplateTagLibSpec extends Specification implements TagLibUnitTest<Templat
 
         and:
         json[0].contains("ala.svg")
+    }
+    
+    void "isUrlActivePage returns true when url matches request URI with query string"() {
+        setup:
+        setupRequest()
+        String testUrl = "/current/page?id=123"
+
+        expect:
+        tagLib.isUrlActivePage(testUrl)
+    }
+
+    void "isUrlActivePage returns false when query string does not match"() {
+        setup:
+        setupRequest()
+        String testUrl = "/current/page?id=1y1"
+
+        expect:
+        !tagLib.isUrlActivePage(testUrl)
+    }
+
+    void "isUrlActivePage returns false when url does not match"() {
+        setup:
+        setupRequest()
+        String testUrl = "/other/page?id=123"
+
+        expect:
+        !tagLib.isUrlActivePage(testUrl)
+    }
+
+    void "isUrlActivePage ignores query string when ignoreQueryString is true"() {
+        setup:
+        setupRequest()
+        String testUrl = "current/page?id=xyz"
+
+        expect:
+        tagLib.isUrlActivePage(testUrl, true)
+    }
+
+    void "isUrlActivePage handles malformed URL gracefully"() {
+        setup:
+        setupRequest()
+        String testUrl = "invalid url"
+
+        expect:
+        !tagLib.isUrlActivePage(testUrl)
+    }
+
+    void "isUrlActivePage returns false when url is null"() {
+        setup:
+        setupRequest()
+
+        expect:
+        !tagLib.isUrlActivePage(null)
+    }
+
+    private void setupRequest() {
+        request.method = 'GET'
+        request.requestURI = '/current/page'
+        request.forwardURI = '/current/page'
+        request.contextPath = ''
+        request.setParameter('id', '123')
     }
 }
