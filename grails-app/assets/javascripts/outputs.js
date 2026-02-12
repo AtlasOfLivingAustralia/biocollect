1fff66864b46c9b4bd07f7253ee17a6b876eab09
temi <temi@users.noreply.github.com>
2026-02-12 13:39:23 +1100

    AtlasOfLivingAustralia/ecodata#1084 - better error handling if virus detected

--- grails-app/assets/javascripts/outputs.js
+++ grails-app/assets/javascripts/outputs.js
@@ -217,7 +217,14 @@ ko.bindingHandlers.photoPointUpload = {
             }
 
         }).on('fileuploadfail', function(e, data) {
-            error(data.errorThrown);
+            var jqXHR = data.jqXHR;
+            if (jqXHR && jqXHR.status === 422) {
+                var resp = jqXHR.responseJSON || {message: "File upload could not be processed. Possible virus detected."};
+                error(resp.message);
+            }
+            else {
+                error(data.errorThrown);
+            }
         });
 
         ko.applyBindingsToDescendants(innerContext, element);
@@ -331,7 +338,14 @@ ko.bindingHandlers.imageUpload = {
                     file && biocollect.utils.readDocument(file).then(biocollect.utils.saveDocument).then(biocollect.utils.fetchDocument).then(addToViewModel);
                 },
                 function () {
-                    error(data.errorThrown);
+                    var jqXHR = data.jqXHR;
+                    if (jqXHR && jqXHR.status === 422) {
+                        var resp = jqXHR.responseJSON || {message: "File upload could not be processed. Possible virus detected."};
+                        error(resp.message);
+                    }
+                    else {
+                        error(data.errorThrown);
+                    }
                 });
             }
             else {
@@ -441,7 +455,14 @@ ko.bindingHandlers.fileUploadWithProgress = {
             }
 
         }).on('fileuploadfail', function (e, data) {
-            error(data.errorThrown);
+            var jqXHR = data.jqXHR;
+            if (jqXHR && jqXHR.status === 422) {
+                var resp = jqXHR.responseJSON || {message: "File upload could not be processed. Possible virus detected."};
+                error(resp.message);
+            }
+            else {
+                error(data.errorThrown);
+            }
         });
 
         ko.applyBindingsToDescendants(innerContext, element);
