7c43353a7a1bfab1f42bcda017951e1c787022ef
temi <temi@users.noreply.github.com>
2026-02-12 17:31:47 +1100

    AtlasOfLivingAustralia/ecodata#1084 - fixed for online image upload

--- grails-app/assets/javascripts/outputs.js
+++ grails-app/assets/javascripts/outputs.js
@@ -349,7 +349,14 @@ ko.bindingHandlers.imageUpload = {
                 });
             }
             else {
-                error(data.errorThrown);
+                var jqXHR = data.jqXHR;
+                if (jqXHR && jqXHR.status === 422) {
+                    var resp = jqXHR.responseJSON || {message: "File upload could not be processed. Possible virus detected."};
+                    error(resp.message);
+                }
+                else {
+                    error(data.errorThrown);
+                }
             }
 
             window.decreaseAsyncCounter && window.decreaseAsyncCounter();
