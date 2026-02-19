b2a2e7661ae7d6b729ea3e89cb0e73ae68a30fca
sal153 <joseph.salomon@csiro.au>
2026-02-19 11:34:49 +1100

    commit script updates #1866

--- src/main/scripts/releases/8.3/adhoc/fixCoralWatchRecords.js
+++ src/main/scripts/releases/8.3/adhoc/fixCoralWatchRecords.js
@@ -6,29 +6,23 @@ var dryRun = false;
 
 var FROM = "Dive centre";
 var TO   = "Dive Centre";
+var PROJECT_ID = "9c55416c-f56a-4917-a65e-da1d64a851f7";
 
 var matched = 0;
 var updated = 0;
 var audited = 0;
 var errors  = 0;
 
-function getProjectIdFromActivity(activityId) {
-    if (!activityId) return null;
-    var a = db.activity.findOne({ activityId: activityId }, { projectId: 1 });
-    return (a && a.projectId) ? a.projectId : null;
-}
-
 var query = {
+    projectId: PROJECT_ID,
     status: "active",
     "data.groupType": FROM
 };
 
-db.output.find(query, { outputId: 1, activityId: 1, data: 1 }).forEach(function (output) {
+db.output.find(query, { outputId: 1, data: 1 }).forEach(function (output) {
     matched++;
 
     try {
-        if (!output.data || output.data.groupType !== FROM) return;
-
         if (!dryRun) {
             var result = db.output.updateOne(
                 { outputId: output.outputId, "data.groupType": FROM },
@@ -38,8 +32,7 @@ db.output.find(query, { outputId: 1, activityId: 1, data: 1 }).forEach(function 
             if (result && result.modifiedCount) {
                 updated++;
 
-                var projectId = getProjectIdFromActivity(output.activityId);
-                audit(output, output.outputId, "au.org.ala.ecodata.Output", userId, projectId, "Update");
+                audit(output, output.outputId, "au.org.ala.ecodata.Output", userId, PROJECT_ID, "Update");
                 audited++;
             }
         }
