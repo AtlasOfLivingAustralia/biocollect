c66e8dfc674a321a25d8e8556680573995f04845
temi <temi@users.noreply.github.com>
2026-03-13 08:02:07 +1100

    - getting functional test to work

--- src/test/js/integration/specs/pwa/InstallationSpec.js
+++ src/test/js/integration/specs/pwa/InstallationSpec.js
@@ -48,7 +48,6 @@ describe("Application installation Spec", function () {
     });
 
     it("submit record offline and publish it when network returns", async function () {
-        console.log(url);
         await pwaAppPage.start();
         await addBioActivityPage.takeScreenShot("openProjectAndTakeItOffline");
         await pwaAppPage.viewProject(project);
@@ -119,7 +118,6 @@ describe("Application installation Spec", function () {
     });
 
     it("submit record offline and choose a site on map and publish it when network returns", async function () {
-        console.log(url);
         let getStarted = await pwaAppPage.getStarted;
         if (getStarted && (await getStarted.isDisplayed())) {
             await pwaAppPage.start();
