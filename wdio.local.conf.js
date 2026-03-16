5522e5fa107078227d0a3e8b73fc1054acb8e7d5
temi <temi@users.noreply.github.com>
2026-03-16 17:51:48 +1100

    increased view port

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -8,12 +8,14 @@ const config = {
         //     './src/test/js/integration/specs/pwa/InstallationSpec.js'
         // ],
         maxInstances: 1,
+        maxInstancesPerCapability: 1,
         // services: ['devtools'],
         capabilities: [{
+            maxInstances: 1,
             browserName: 'chrome',
             'goog:chromeOptions': {
                 // args: ['--auto-open-devtools-for-tabs','disable-gpu']
-                args: ['headless', 'disable-gpu', '--window-size=1280,1024']
+                // args: ['headless', 'disable-gpu', '--window-size=3000,3000']
                 // args: ['--auto-open-devtools-for-tabs', 'disable-gpu', '--window-size=2000,2000']
             },
             'wdio:chromedriverOptions': {
