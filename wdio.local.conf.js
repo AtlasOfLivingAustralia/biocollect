fdb994632b78c07ef429732a05cf99d37baf3f5e
chrisala <chris.godwin.ala@gmail.com>
2026-05-29 16:37:53 +1000

    Trying different chromedriver options #1938

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -15,7 +15,7 @@ const config = {
             browserName: 'chrome',
             'goog:chromeOptions': {
                 // args: ['--auto-open-devtools-for-tabs','disable-gpu']
-                args: ['headless', 'disable-gpu', '--window-size=3000,3000']
+                args: ['headless', 'disable-gpu', 'window-size=3000,3000', 'disable-dev-shm-usage', '--headless', '--disable-gpu', '--disable-dev-shm-usage']
                 // args: ['--auto-open-devtools-for-tabs', 'disable-gpu', '--window-size=3000,3000']
             },
             'wdio:chromedriverOptions': {
