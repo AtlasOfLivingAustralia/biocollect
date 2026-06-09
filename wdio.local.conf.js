0d3b82f40b9a1101fed0e158d346909f480fe0e5
sal153 <joseph.salomon@csiro.au>
2026-06-09 16:20:22 +1000

    Removed hardcoded chromedriver binary override and revert node version to 24 #1954

--- wdio.local.conf.js
+++ wdio.local.conf.js
@@ -17,9 +17,6 @@ const config = {
                 // args: ['--auto-open-devtools-for-tabs','disable-gpu']
                 args: ['headless', 'disable-gpu', 'window-size=3000,3000', 'disable-dev-shm-usage', 'no-sandbox', '--headless', '--disable-gpu', '--window-size=3000,3000', '--disable-dev-shm-usage', '--no-sandbox']
                 // args: ['--auto-open-devtools-for-tabs', 'disable-gpu', '--window-size=3000,3000']
-            },
-            'wdio:chromedriverOptions': {
-                binary: require('chromedriver').path
             }
         }],
         testConfig: {
